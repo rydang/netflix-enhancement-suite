99d9ee0b249eb3b187e938a0d79179c9a36a045e
Ryan Dang <rydang@umich.edu>
2019-06-12 19:07:27 -0400

    saved scrap

--- scrap.js
+++ scrap.js
@@ -1,12 +1,17 @@
 // ellipsize-text **** SELECT TITLE
-
+console.log('testing');
 const title = document.querySelector('.ellipsize-text').innerText;
 let section = 'search'
-let type = 'tv'
+let type = 'tv' 
 let URL = new URL(`https://api.themoviedb.org/3/${section}/${type}`)
 let params = {
     api_key:'295842393e648bbfdc4b797075d713f5',
-    query: 
-}
+    query: title,
+    page: 1
+};
+URL.search = new URLSearchParams(params);
 
-api_key=295842393e648bbfdc4b797075d713f5&language=en-US&query=the%20office&page=1'
+fetch(URL).then((response)=>{
+    console.log(response.json());
+});
+
