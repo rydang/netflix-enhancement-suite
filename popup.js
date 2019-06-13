46e6db8e8c295e5973a36a52143d5258604b620b
Alfredo Alpizar <Fredo@Dipnas-MacBook.local>
2019-06-13 09:35:34 -0400

    added css to popup

--- popup.js
+++ popup.js
@@ -1,33 +1,37 @@
 
-function getRoles(actorIds, actorNames, api_key) {
-  for(let i = 0; i < actorIds.length; i++) {
-    const person_id = actorIds[i];
-    fetch(`https://api.themoviedb.org/3/person/${person_id}/combined_credits?api_key=${api_key}`)
+function getRoles(actorId, api_key) {
+    fetch(`https://api.themoviedb.org/3/person/${actorId}/combined_credits?api_key=${api_key}`)
     .then(response => response.json())
     .then(parsedJson => {
       // chrome.storage.sync.set({`${cast}_roles`: parsedJson.cast})
+      parsedJson.cast.forEach((ele)=>{
+        console.log(ele)
+      })
       console.log(parsedJson.cast);
     });
   }
-}
 
 function getActors(showDetails, type, api_key) {
   fetch(`https://api.themoviedb.org/3/${type}/${showDetails['id']}/credits?api_key=${api_key}`)
   .then(response => response.json())
   .then(parsedJson => {
-    const cast = [];
-    const castIds = [];
+    // const cast = [];
+    // const castIds = [];
     for(let i = 0; i < parsedJson.cast.length; i += 1) {
-      cast.push(parsedJson.cast[i]['name']);
-      castIds.push(parsedJson.cast[i]['id']);
+      // cast.push(parsedJson.cast[i]['name']);
+      // castIds.push(parsedJson.cast[i]['id']);
       let div = document.createElement('div');
       div.classList.add('person')
-      div.innerHTML = `<h4>${parsedJson.cast[i].name} as ${parsedJson.cast[i].character}</h4><img src='${'http://image.tmdb.org/t/p/w185'+parsedJson.cast[i]['profile_path']}'>`
+      div.id = parsedJson.cast[i]['id'];
+      div.innerHTML = `<h4 id='${parsedJson.cast[i]['id']}'>${parsedJson.cast[i].name} as ${parsedJson.cast[i].character}</h4><img id='${parsedJson.cast[i]['id']}' src='${'http://image.tmdb.org/t/p/w185'+parsedJson.cast[i]['profile_path']}'>`
       // img.src = 'http://image.tmdb.org/t/p/w185'+parsedJson.cast[i]['profile_path']
+      div.addEventListener('click', function(event) {
+        //console.log(event.target.id);
+        getRoles(event.target.id, api_key);
+      })
       document.querySelector('#actors').appendChild(div);
 
     }
-    getRoles(castIds, cast, api_key);
   });
 }
 
@@ -36,7 +40,7 @@ function sendMDBRequest(title, type) {
   let api_key = '295842393e648bbfdc4b797075d713f5';
   let URL = `https://api.themoviedb.org/3/${section}/${type}?api_key=${api_key}&query=${title}`
   
-  return fetch(URL).then((response)=>{
+  fetch(URL).then((response)=>{
       return response.json();
   })
   .then((parsedJson)=>{
